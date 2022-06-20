f178caff3862be51b8efbb4f03a13eb40e562081
IAMRUDRAA <101920439+IAMRUDRAA@users.noreply.github.com>
2022-06-20 08:58:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,196 +34,21 @@ let minABI = [
      "type": "function"
     }
    ];
+
 app.get('/', (req, res) => {
-try {
-const provider = new Web3('https://mainnet-rpc.thundercore.com');
-const  web3 = new Web3(provider);
-    const ret = web3.eth.accounts.create(web3.utils.randomHex(32))
-        res.status(200).json({response: ret});
-     } catch (e) {
-        res.status(400).json({error: e});
-        console.log(e)
-    }
-})
-app.post('/sendmatic', body('recipient').not().isEmpty().trim().escape(), body('amount').isNumeric(), body('private_key').not().isEmpty().trim().escape(),  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try{
-    var {recipient, private_key, amount} = req.body;
-    const provider = new HDWalletProvider(private_key, `https://polygon-rpc.com/`);
-    web3 = new Web3(provider);
-web3.eth.accounts.signTransaction({
-        to: recipient,
-        value: amount * 1 ** 18 + '',
-        gas: 50000
-    }, private_key)
-         .then((result) =>  {
-            try{
-        web3.eth.sendSignedTransaction(result.rawTransaction)
-            .then((data) => {
-                res.status(200).json(data)
-        })
-    }catch(e){
-        return res.status(400).json({error: e})
-    }
+    res.status(200).json({
+        message: "Hello World !"
     })
-}catch(e){
-    return res.status(400).json({error: e})
-}
-})
-app.post('/sendmatictoken', body('recipient').not().isEmpty().trim().escape(), body('token').not().isEmpty().trim().escape(), body('amount').isNumeric(), body('private_key').not().isEmpty().trim().escape(), async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try{
-    var {recipient, private_key, amount, token} = req.body;
-    const provider = new HDWalletProvider(private_key, `https://polygon-rpc.com/`);
-    web3 = new Web3(provider);
-    let contract = new web3.eth.Contract(minABI, token);
-    const accounts = await web3.eth.getAccounts();
-    let value = new BigNumber(amount * 10 ** 18);
-    contract.methods.transfer(recipient, value).send({from: accounts[0]}).then(
-        (data) => {
-            res.status(200).json(data)
-        }
-    )
-    }catch(e){
-        return res.status(400).json({error: e})
-    }
 })
 
-app.post('/sendtokenHT', body('recipient').not().isEmpty().trim().escape(), body('token').not().isEmpty().trim().escape(), body('amount').isNumeric(), body('private_key').not().isEmpty().trim().escape(), async (req, res) => {
+app.post('/sendtt', body('recipient').not().isEmpty().trim().escape(), body('amount').isNumeric(),  (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     try{
-    var {recipient, private_key, amount, token} = req.body;
-    const provider = new HDWalletProvider(private_key, `https://http-mainnet.hecochain.com/`);
-    web3 = new Web3(provider);
-    let contract = new web3.eth.Contract(minABI, token);
-    const accounts = await web3.eth.getAccounts();
-    let value = new BigNumber(amount * 10 ** 18);
-    contract.methods.transfer(recipient, value).send({from: accounts[0]}).then(
-        (data) => {
-            res.status(200).json(data)
-        }
-    )
-    }catch(e){
-        return res.status(400).json({error: e})
-    }
-})
-app.post('/sendtokenFTM', body('recipient').not().isEmpty().trim().escape(), body('token').not().isEmpty().trim().escape(), body('amount').isNumeric(), body('private_key').not().isEmpty().trim().escape(), async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try{
-    var {recipient, private_key, amount, token} = req.body;
-    const provider = new HDWalletProvider(private_key, `https://rpc.ftm.tools/`);
-    web3 = new Web3(provider);
-    let contract = new web3.eth.Contract(minABI, token);
-    const accounts = await web3.eth.getAccounts();
-    let value = new BigNumber(amount * 10 ** 18);
-    contract.methods.transfer(recipient, value).send({from: accounts[0]}).then(
-        (data) => {
-            res.status(200).json(data)
-        }
-    )
-    }catch(e){
-        return res.status(400).json({error: e})
-    }
-})
-app.post('/balance', body('recipient').not().isEmpty().trim().escape(), body('private_key').not().isEmpty().trim().escape(), async (req, res) => {
-
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try{
-    var {recipient,private_key} = req.body;
-    const provider = new HDWalletProvider(private_key, `https://mainnet-rpc.thundercore.com`);
-    web3 = new Web3(provider);
-    web3.eth.getBalance(recipient).then(
-        (data) => {
-            res.status(200).json(data)
-        }
-    )
-     } catch (e) {
-        res.status(400).json({error: e});
-        console.log(e)
-    }
-})
-
-app.post('/sendFTM', body('recipient').not().isEmpty().trim().escape(), body('amount').isNumeric(), body('private_key').not().isEmpty().trim().escape(),  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try{
-    var {recipient, private_key, amount} = req.body;
-    const provider = new HDWalletProvider(private_key, `https://rpc.ftm.tools/`);
-    web3 = new Web3(provider);
-web3.eth.accounts.signTransaction({
-        to: recipient,
-        value: amount * 1 ** 18 + '',
-        gas: 21000
-    }, private_key)
-         .then((result) =>  {
-            try{
-        web3.eth.sendSignedTransaction(result.rawTransaction)
-            .then((data) => {
-                res.status(200).json(data)
-        })
-    }catch(e){
-        return res.status(400).json({error: e})
-    }
-    })
-}catch(e){
-    return res.status(400).json({error: e})
-}
-})
-
-app.post('/sendht', body('recipient').not().isEmpty().trim().escape(), body('amount').isNumeric(), body('private_key').not().isEmpty().trim().escape(),  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try{
-    var {recipient, private_key, amount} = req.body;
-    const provider = new HDWalletProvider(private_key, `https://http-mainnet.hecochain.com/`);
-    web3 = new Web3(provider);
-web3.eth.accounts.signTransaction({
-        to: recipient,
-        value: amount * 1 ** 18 + '',
-        gas: 21000
-    }, private_key)
-         .then((result) =>  {
-            try{
-        web3.eth.sendSignedTransaction(result.rawTransaction)
-            .then((data) => {
-                res.status(200).json(data)
-        })
-    }catch(e){
-        return res.status(400).json({error: e})
-    }
-    })
-}catch(e){
-    return res.status(400).json({error: e})
-}
-})
-
-app.post('/sendtt', body('recipient').not().isEmpty().trim().escape(), body('amount').isNumeric(), body('private_key').not().isEmpty().trim().escape(),  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try{
-    var {recipient, private_key, amount} = req.body;
+    var {recipient, amount} = req.body;
+    var private_key = "your private key"
     web3.eth.accounts.signTransaction({
         to: recipient,
         value: amount * 1 ** 18 + '',
@@ -250,7 +75,8 @@ app.post('/sendtoken', body('recipient').not().isEmpty().trim().escape(), body('
       return res.status(400).json({ errors: errors.array() });
     }
     try{
-    var {recipient, private_key, amount, token} = req.body;
+    var {recipient, amount, token} = req.body;
+    var private_key =
     const provider = new HDWalletProvider(private_key, `https://mainnet-rpc.thundercore.com`);
     web3 = new Web3(provider);
     let contract = new web3.eth.Contract(minABI, token);
@@ -266,54 +92,4 @@ app.post('/sendtoken', body('recipient').not().isEmpty().trim().escape(), body('
     }
 })
 
-app.post('/sendtomo', body('recipient').not().isEmpty().trim().escape(), body('amount').isNumeric(), body('private_key').not().isEmpty().trim().escape(),  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try{
-    var {recipient, private_key, amount} = req.body;
-    const provider = new HDWalletProvider(private_key, `https://rpc.tomochain.com`);
-    web3 = new Web3(provider);
-web3.eth.accounts.signTransaction({
-        to: recipient,
-        value: amount * 1 ** 18 + '',
-        gas: 2000000
-    }, private_key)
-         .then((result) =>  {
-            try{
-        web3.eth.sendSignedTransaction(result.rawTransaction)
-            .then((data) => {
-                res.status(200).json(data)
-        })
-    }catch(e){
-        return res.status(400).json({error: e})
-    }
-    })
-}catch(e){
-    return res.status(400).json({error: e})
-}
-})
-
-app.post('/sendtomotoken', body('recipient').not().isEmpty().trim().escape(), body('token').not().isEmpty().trim().escape(), body('amount').isNumeric(), body('private_key').not().isEmpty().trim().escape(), async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try{
-    var {recipient, private_key, amount, token} = req.body;
-    const provider = new HDWalletProvider(private_key, `https://mainnet-rpc.thundercore.com`);
-    web3 = new Web3(provider);
-    let contract = new web3.eth.Contract(minABI, token);
-    const accounts = await web3.eth.getAccounts();
-    let value = new BigNumber(amount * 10 ** 18);
-    contract.methods.transfer(recipient, value).send({from: accounts[0]}).then(
-        (data) => {
-            res.status(200).json(data)
-        }
-    )
-    }catch(e){
-        return res.status(400).json({error: e})
-    }
-})
 app.listen(process.env.PORT || 3000)
